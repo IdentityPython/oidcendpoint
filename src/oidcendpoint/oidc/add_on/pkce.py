e1d572ba81c10b8bad7b0501eabfaab80fc3d7ad
Nikos Sklikas <nsklikas@admin.grnet.gr>
2020-08-19 11:47:28 +0300

    Refactor pkce

--- src/oidcendpoint/oidc/add_on/pkce.py
+++ src/oidcendpoint/oidc/add_on/pkce.py
@@ -7,7 +7,22 @@ from oidcmsg.oidc import TokenErrorResponse
 
 LOGGER = logging.getLogger(__name__)
 
-CC_METHOD = {"S256": hashlib.sha256, "S384": hashlib.sha384, "S512": hashlib.sha512}
+
+def hash_fun(f):
+    def wrapper(code_verifier):
+        _h = f(code_verifier.encode("ascii")).digest()
+        _cc = b64e(_h)
+        return _cc.decode("ascii")
+
+    return wrapper
+
+
+CC_METHOD = {
+    "plain": lambda x: x,
+    "S256": hash_fun(hashlib.sha256),
+    "S384": hash_fun(hashlib.sha384),
+    "S512": hash_fun(hashlib.sha512),
+}
 
 
 def post_authn_parse(request, client_id, endpoint_context, **kwargs):
@@ -22,37 +37,35 @@ def post_authn_parse(request, client_id, endpoint_context, **kwargs):
     if isinstance(request, AuthorizationErrorResponse):
         return request
 
-    if endpoint_context.args["pkce"]["essential"] is True:
-        if "code_challenge" not in request:
-            return AuthorizationErrorResponse(
-                error="invalid_request",
-                error_description="Missing required code_challenge"
-            )
-        if "code_challenge_method" not in request:
-            if "plain" not in endpoint_context.args["pkce"]["code_challenge_method"]:
-                return AuthorizationErrorResponse(
-                    error="invalid_request",
-                    error_description="No support for code_challenge_method=plain"
-                )
+    if (
+        endpoint_context.args["pkce"]["essential"]
+        and "code_challenge" not in request
+    ):
+        return AuthorizationErrorResponse(
+            error="invalid_request",
+            error_description="Missing required code_challenge",
+        )
 
-            request["code_challenge_method"] = "plain"
-    else:  # May or may not
-        if "code_challenge" in request:
-            if "code_challenge_method" not in request:
-                if (
-                    "plain"
-                    not in endpoint_context.args["pkce"]["code_challenge_method"]
-                ):
-                    return AuthorizationErrorResponse(
-                        error="invalid_request",
-                        error_description="No support for code_challenge_method=plain"
-                    )
+    if "code_challenge_method" not in request:
+        request["code_challenge_method"] = "plain"
 
-                request["code_challenge_method"] = "plain"
+    if (
+        request["code_challenge_method"]
+        not in endpoint_context.args["pkce"]["code_challenge_methods"]
+    ):
+        return AuthorizationErrorResponse(
+            error="invalid_request",
+            error_description="Unsupported code_challenge_method={}".format(
+                request["code_challenge_method"]
+            ),
+        )
+
     return request
 
 
-def verify_code_challenge(code_verifier, code_challenge, code_challenge_method="S256"):
+def verify_code_challenge(
+    code_verifier, code_challenge, code_challenge_method="S256"
+):
     """
     Verify a PKCE (RFC7636) code challenge.
 
@@ -61,16 +74,12 @@ def verify_code_challenge(code_verifier, code_challenge, code_challenge_method="
     :param code_challenge: The transformed verifier used as challenge
     :return:
     """
-    _h = CC_METHOD[code_challenge_method](code_verifier.encode("ascii")).digest()
-    _cc = b64e(_h)
-    if _cc.decode("ascii") != code_challenge:
+    if CC_METHOD[code_challenge_method](code_verifier) != code_challenge:
         LOGGER.error("PKCE Code Challenge check failed")
-        return TokenErrorResponse(
-            error="invalid_grant",
-            error_description="PKCE check failed"
-        )
+        return False
 
     LOGGER.debug("PKCE Code Challenge check succeeded")
+    return True
 
 
 def post_token_parse(request, client_id, endpoint_context, **kwargs):
@@ -83,29 +92,30 @@ def post_token_parse(request, client_id, endpoint_context, **kwargs):
     if isinstance(request, AuthorizationErrorResponse):
         return request
 
-    if "code_verifier" in request:
-        try:
-            _info = endpoint_context.sdb[request["code"]]
-        except KeyError:
+    try:
+        _info = endpoint_context.sdb[request["code"]]
+    except KeyError:
+        return TokenErrorResponse(
+            error="invalid_grant", error_description="Unknown access grant"
+        )
+    _authn_req = _info["authn_req"]
+
+    if "code_challenge" in _authn_req:
+        if "code_verifier" not in request:
             return TokenErrorResponse(
                 error="invalid_grant",
-                error_description="Unknown access grant"
+                error_description="Missing code_verifier",
             )
 
-        _authn_req = _info["authn_req"]
-        if "code_challenge" in _authn_req:
-            try:
-                _method = _info["authn_req"]["code_challenge_method"]
-            except KeyError:
-                _method = "S256"
+        _method = _info["authn_req"]["code_challenge_method"]
 
-            verify_code_challenge(
-                request["code_verifier"], _info["authn_req"]["code_challenge"], _method
-            )
-        else:
+        if not verify_code_challenge(
+            request["code_verifier"],
+            _info["authn_req"]["code_challenge"],
+            _method,
+        ):
             return TokenErrorResponse(
-                error="invalid_grant",
-                error_description="Missing code_challenge in authorization request"
+                error="invalid_grant", error_description="PKCE check failed"
             )
 
     return request
@@ -114,11 +124,18 @@ def post_token_parse(request, client_id, endpoint_context, **kwargs):
 def add_pkce_support(endpoint, **kwargs):
     endpoint["authorization"].post_parse_request.append(post_authn_parse)
 
-    # Set defaults
     if "essential" not in kwargs:
         kwargs["essential"] = False
-    if "code_challenge" not in kwargs:
-        kwargs["code_challenge"] = list(CC_METHOD.keys())
+
+    code_challenge_methods = kwargs.get(
+        "code_challenge_methods", CC_METHOD.keys()
+    )
+
+    kwargs["code_challenge_methods"] = {}
+    for method in code_challenge_methods:
+        if method not in CC_METHOD:
+            raise ValueError("Unsupported method: {}".format(method))
+        kwargs["code_challenge_methods"][method] = CC_METHOD[method]
 
     endpoint["authorization"].endpoint_context.args["pkce"] = kwargs
 
