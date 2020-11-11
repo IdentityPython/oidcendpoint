ec006ad370ebca7f28fa48d11c0b3e34e2cbcd29
Antonis Angelakis <angelakis@grnet.gr>
2020-11-11 14:49:48 +0200

    Improve TokenCoop tests and add more

--- tests/test_35_oidc_token_coop_endpoint.py
+++ tests/test_35_oidc_token_coop_endpoint.py
@@ -7,6 +7,7 @@ from cryptojwt.key_jar import build_keyjar
 from oidcmsg.oidc import AccessTokenRequest
 from oidcmsg.oidc import AuthorizationRequest
 from oidcmsg.oidc import RefreshAccessTokenRequest
+from oidcmsg.oidc import ResponseMessage
 
 from oidcendpoint import JWT_BEARER
 from oidcendpoint.client_authn import verify_client
@@ -205,8 +206,10 @@ class TestEndpoint(object):
     def test_errors_in_grant_types_supported(self, conf, grant_types_supported):
         token_conf = conf["endpoint"]["token"]
         token_conf["kwargs"]["grant_types_supported"] = grant_types_supported
-        with pytest.raises(Exception):
+        with pytest.raises(Exception) as exception_info:
             EndpointContext(conf)
+        assert exception_info.typename == "ProcessError"
+        assert "Token Endpoint" in str(exception_info.value)
 
     def test_parse(self):
         session_id = setup_session(self.endpoint.endpoint_context, AUTH_REQ, uid="user")
@@ -422,3 +425,49 @@ class TestEndpoint(object):
         assert "error_description" in _resp
         assert _resp["error"] == "invalid_request"
         assert _resp["error_description"] == "Unsupported grant_type: refresh_token"
+
+    def test_custom_grant_class(self, conf):
+        """
+        Register a custom grant type supported and see if it works as it should.
+        """
+        class CustomGrant:
+            def __init__(self, endpoint, config=None):
+                self.endpoint = endpoint
+
+            def post_parse_request(self, request, client_id="", **kwargs):
+                request.testvalue = "test"
+                return request
+
+            def process_request(self, request, **kwargs):
+                """
+                All grant types should return a ResponseMessage class or inherit it.
+                """
+                return ResponseMessage(test="successful")
+
+        token_conf = conf["endpoint"]["token"]
+        token_conf["kwargs"]["grant_types_supported"] = {
+            "authorization_code": True,
+            "test_grant": {
+                "class": CustomGrant
+            }
+        }
+        endpoint_context = EndpointContext(conf)
+        token_endpoint = endpoint_context.endpoint["token"]
+        token_endpoint.client_authn_method = [None]
+        endpoint_context.cdb["client_1"] = {
+            "client_secret": "hemligt",
+            "redirect_uris": [("https://example.com/cb", None)],
+            "client_salt": "salted",
+            "endpoint_auth_method": "client_secret_post",
+            "response_types": ["code", "token", "code id_token", "id_token"],
+        }
+        endpoint_context.keyjar.import_jwks(CLIENT_KEYJAR.export_jwks(), "client_1")
+
+        request = dict(grant_type="test_grant", client_id="client_1")
+
+        parsed_request = token_endpoint.parse_request(request)
+        assert parsed_request.testvalue == "test"
+
+        response = token_endpoint.process_request(parsed_request)
+        assert "test" in response
+        assert response["test"] == "successful"
