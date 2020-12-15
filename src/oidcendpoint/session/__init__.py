743b67a46740d179ed9722c3d05bf8b6227629d5
roland <roland@catalogix.se>
2020-12-15 15:34:26 +0100

    A simple ':' as divider does not work.

--- src/oidcendpoint/session/__init__.py
+++ src/oidcendpoint/session/__init__.py
@@ -2,11 +2,11 @@ from typing import List
 
 
 def session_key(*args) -> str:
-    return ':'.join(args)
+    return ';;'.join(args)
 
 
 def unpack_session_key(key: str) -> List[str]:
-    return key.split(':')
+    return key.split(';;')
 
 
 class Revoked(Exception):
